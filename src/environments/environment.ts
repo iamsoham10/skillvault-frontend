1df25ee9ee4a07e49af09f9629f061925ec4b51f
Soham Chitale <chitalesoham106@gmail.com>
2025-04-30 22:23:34 +0530

    updated endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  LOGIN_API: 'http://localhost:3000/api/user/login',
-  SIGNUP_API: 'http://localhost:3000/api/user/register',
-  OTP_API: 'http://localhost:3000/api/user/verify-otp',
-  NEW_ACCESS_TOKEN_API: 'http://localhost:3000/api/user/refresh-token',
-  COLLECTION_API: 'http://localhost:3000/api/collection/',
-  RESOURCE_API: 'http://localhost:3000/api/resource/',
-  AUTH_API: 'http://localhost:3000/api/auth/',
+  LOGIN_API: 'https://skillvault-backend-hnww.onrender.com/api/user/login',
+  SIGNUP_API: 'https://skillvault-backend-hnww.onrender.com/api/user/register',
+  OTP_API: 'https://skillvault-backend-hnww.onrender.com/api/user/verify-otp',
+  NEW_ACCESS_TOKEN_API: 'https://skillvault-backend-hnww.onrender.com/api/user/refresh-token',
+  COLLECTION_API: 'https://skillvault-backend-hnww.onrender.com/api/collection/',
+  RESOURCE_API: 'https://skillvault-backend-hnww.onrender.com/api/resource/',
+  AUTH_API: 'https://skillvault-backend-hnww.onrender.com/api/auth/',
 };
